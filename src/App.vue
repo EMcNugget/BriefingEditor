9ef96d263b821600067ed6994e3e1c17b9dc6cd0
EMcNugget <ethan.d.seys@gmail.com>
2023-05-15 14:22:26 -0500

    Forms for tasks

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,15 @@
     :theme="selectedTheme === 'Dark' ? theme : null"
     :theme-overrides="selectedTheme === 'Dark' ? themeOverrides : null"
   >
-    <div></div>
+    <div>
+      <briefing-editor />
+    </div>
   </n-config-provider>
 </template>
 
 <script setup lang="ts">
 import type { GlobalTheme, GlobalThemeOverrides } from "naive-ui";
+import BriefingEditor from "./components/BriefingEditor.vue";
 import { NConfigProvider } from "naive-ui";
 import { useThemeStore } from "./stores/state";
 import { ref } from "vue";
--- src/components/BriefingEditor.vue
+++ src/components/BriefingEditor.vue
@@ -1,6 +1,58 @@
 <template>
-
+  <div class="m-5 flex flex-col w-1/3">
+    <n-input placeholder="Enter a sortie name" class="mb-14" />
+    <n-grid :x-gap="12" :y-gap="50" :cols="1">
+      <n-grid-item>
+        <n-h4>Situation</n-h4>
+        <n-input
+          v-model:value="situation"
+          class="h-full"
+          type="textarea"
+          placeholder="Situation"
+          :resizable="false"
+        />
+      </n-grid-item>
+      <n-grid-item>
+        <n-h4>Blue Coalition Tasks</n-h4>
+        <n-input
+          v-model:value="blueTasks"
+          class="h-full"
+          type="textarea"
+          placeholder="Blue Coalition Tasks"
+          :resizable="false"
+        />
+      </n-grid-item>
+      <n-grid-item>
+        <n-h4>Red Coalition Tasks</n-h4>
+        <n-input
+          v-model:value="redTasks"
+          class="h-full"
+          type="textarea"
+          placeholder="Red Coalition Tasks"
+          :resizable="false"
+          f
+        />
+      </n-grid-item>
+      <n-grid-item>
+        <n-h4>Neutral Coalition Tasks</n-h4>
+        <n-input
+          v-model:value="neutralTasks"
+          class="h-full"
+          type="textarea"
+          placeholder="Neutral Coalition Tasks"
+          :resizable="false"
+        />
+      </n-grid-item>
+    </n-grid>
+  </div>
 </template>
 
 <script setup lang="ts">
+import { NInput, NGrid, NGridItem, NH4 } from "naive-ui";
+import { ref } from "vue";
+
+const situation = ref("");
+const blueTasks = ref("");
+const redTasks = ref("");
+const neutralTasks = ref("");
 </script>
